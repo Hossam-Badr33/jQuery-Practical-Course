dabaaede4e83ce7baffb03ca5cbe38660e780434
Hossam-Badr33 <dream.onlinebusiness@gmail.com>
2022-07-07 21:36:58 +0300

    create poup with options

---
+++ 5-create poup with options/plugins.js
@@ -0,0 +1,78 @@
+$(document).ready(function () {
+  $(".content *").css({
+    paddingTop: $(".nav").innerHeight() - 30,
+  });
+  $(".list").click(function (e) {
+    e.preventDefault();
+    /*var select = $("#" + $(this).attr("data-scroll") + "-section");*/
+    var select = $("#" + $(this).data("scroll"));
+    $("html, body").animate(
+      {
+        scrollTop: select.offset().top + 1,
+      },
+      500
+    );
+  });
+  /* add active class to anchor links in nav */
+  $(".list[data-scroll='home']").find("a").addClass("active");
+  $(".list a").click(function () {
+    //global solution
+    /*
+    $(".nav a").removeClass("active");
+    $(this).addClass("active");
+    */
+    //select certain items
+    $(this)
+      .addClass("active")
+      .parent()
+      .siblings()
+      .find("a")
+      .removeClass("active");
+  });
+  /*sync links with sections */
+  $(window).scroll(function () {
+    $(".section").each(function () {
+      if ($(window).scrollTop() > $(this).offset().top) {
+        var indicator = $(this).attr("id");
+        $(`.list[data-scroll=${indicator}]`)
+          .find("a")
+          .addClass("active")
+          .parent()
+          .siblings()
+          .find("a")
+          .removeClass("active");
+      }
+    });
+    /* up to top .fa*/
+    if ($(window).scrollTop() > 700) {
+      if ($(".up").is(":hidden")) {
+        $(".up").fadeIn(500);
+      }
+    } else {
+      $(".up").fadeOut(500);
+    }
+  });
+  $(".up").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      1000
+    );
+  });
+  /*.popup*/
+  $(".show-btn").click(function () {
+    console.log("clicked");
+    $(".popup").addClass("visible");
+  });
+  $(".popup").click(function () {
+    $(this).removeClass("visible");
+  });
+  $(".popup-inner").click(function (e) {
+    e.stopPropagation();
+  });
+  $(".inner-show-btn").click(function (e) {
+    e.preventDefault();
+    $(".popup").removeClass("visible");
+  });
+});
